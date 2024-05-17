8679fb1f5cca69373514583a09bb9315ec482538
Álvaro Corrochano López <42788636+Corrochano@users.noreply.github.com>
2024-05-17 12:52:21 +0200

    Create helloZED.py
    First of the tutorial Scripts to test the camera API

---
+++ CameraTest/helloZED.py
@@ -0,0 +1,21 @@
+# Source ----> https://www.stereolabs.com/docs/tutorials/hello-zed
+
+# Create a ZED camera object
+zed = sl.Camera()
+
+# Set configuration parameters
+init_params = sl.InitParameters()
+init_params.camera_resolution = sl.RESOLUTION.HD1080
+init_params.camera_fps = 30
+
+# Open the camera
+err = zed.open(init_params)
+if err != sl.ERROR_CODE.SUCCESS:
+    exit()
+
+# Get camera information (serial number)
+zed_serial = zed.get_camera_information().serial_number
+print("Hello! This is my serial number: {}".format(zed_serial))
+
+# Close the camera
+zed.close()
