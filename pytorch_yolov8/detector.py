642ba5d38b1276e801f1173abbb54fa301c7a453
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2024-05-21 13:56:27 +0200

    Add files via upload

--- pytorch_yolov8/detector.py
+++ pytorch_yolov8/detector.py
@@ -202,9 +202,9 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov8n.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', type=str, default='best.pt', help='model.pt path(s)')
     parser.add_argument('--svo', type=str, default=None, help='optional svo file')
-    parser.add_argument('--img_size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--img_size', type=int, default=800, help='inference size (pixels)')
     parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
     opt = parser.parse_args()
 
