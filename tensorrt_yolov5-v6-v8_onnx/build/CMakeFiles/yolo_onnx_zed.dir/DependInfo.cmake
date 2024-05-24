0864a53ccd4bb3ac513936b98b70956944cbf8d8
Iuliusgh <81573719+Iuliusgh@users.noreply.github.com>
2024-05-24 12:01:04 +0200

    Add c++ version

---
+++ tensorrt_yolov5-v6-v8_onnx/build/CMakeFiles/yolo_onnx_zed.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nano/Downloads/zed-sdk-master/object detection/custom detector/cpp/tensorrt_yolov5-v6-v8_onnx/src/GLViewer.cpp" "CMakeFiles/yolo_onnx_zed.dir/src/GLViewer.cpp.o" "gcc" "CMakeFiles/yolo_onnx_zed.dir/src/GLViewer.cpp.o.d"
+  "/home/nano/Downloads/zed-sdk-master/object detection/custom detector/cpp/tensorrt_yolov5-v6-v8_onnx/src/main.cpp" "CMakeFiles/yolo_onnx_zed.dir/src/main.cpp.o" "gcc" "CMakeFiles/yolo_onnx_zed.dir/src/main.cpp.o.d"
+  "/home/nano/Downloads/zed-sdk-master/object detection/custom detector/cpp/tensorrt_yolov5-v6-v8_onnx/src/yolo.cpp" "CMakeFiles/yolo_onnx_zed.dir/src/yolo.cpp.o" "gcc" "CMakeFiles/yolo_onnx_zed.dir/src/yolo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
