2a301556b20a6c880ce3a64f7778f0b4aba5204a
Álvaro Corrochano López <42788636+Corrochano@users.noreply.github.com>
2024-06-11 15:55:55 +0200

    New take power script for vcpugpu and vin

--- Measure/takePower.py
+++ Measure/takePower.py
@@ -11,6 +11,13 @@ allValues[0][1] = {}
 allValues[1][0] = {}
 allValues[1][1] = {}
 
+cpugpuValues = { 0: {}, 1: {} }
+
+cpugpuValues[0][0] = {}
+cpugpuValues[0][1] = {}
+cpugpuValues[1][0] = {}
+cpugpuValues[1][1] = {}
+
 def graficar_lista(valores):
     # Crear los ejes x e y
     x = range(len(valores))
@@ -28,17 +35,19 @@ def graficar_lista(valores):
     plt.ylabel('Consumo (mW)')
 
 def graficar_todos():
-    graficar_15w()
-    graficar_7w()
+    #graficar_15w()
+    #graficar_7w()
 
-    graficar_cpp()
-    graficar_python()
+    #graficar_cpp()
+    #graficar_python()
 
-    graficar_15w_barras()
-    graficar_7w_barras()
+    #graficar_15w_barras()
+    #graficar_7w_barras()
 
     graficar_cpp_barras()
-    graficar_python_barras()
+    #graficar_python_barras()
+
+    graficar_cpp_cpugpu_barras()
     
 def graficar_15w():
 
@@ -176,7 +185,7 @@ def graficar_7w_barras():
 def graficar_cpp_barras():
 
     plt.figure(figsize=(12, 6)) 
-    plt.ylim(1500, 2300)
+    plt.ylim(1500, 7000)
     mean_list = [sum(allValues[1][0][0])/len(allValues[1][0][0]),
                  sum(allValues[1][0][2])/len(allValues[1][0][2]), 
                  sum(allValues[1][0][1])/len(allValues[1][0][1]),
@@ -188,12 +197,36 @@ def graficar_cpp_barras():
             mean_list,    
             color=['red', 'blue', 'green', 'purple','yellow', 'orange'], width=0.5)
     
-    plt.title('Consumo Medio en C++' )
-    plt.xlabel('Red y Lenguaje Usados')
+    plt.title('Consumo Medio en C++ (Vin)' )
+    plt.xlabel('Red y Modo Usados')
     plt.ylabel('Consumo (mW)')
     
     #plt.legend()
     plt.savefig('cppBarras.png')
+    #plt.show()
+    plt.clf()
+
+def graficar_cpp_cpugpu_barras():
+
+    #plt.figure(figsize=(12, 6)) 
+    #plt.ylim(1500, 2000)
+    mean_list = [sum(cpugpuValues[1][0][0])/len(cpugpuValues[1][0][0]),
+                 sum(cpugpuValues[1][0][2])/len(cpugpuValues[1][0][2]), 
+                 sum(cpugpuValues[1][0][1])/len(cpugpuValues[1][0][1]),
+                 sum(cpugpuValues[1][1][0])/len(cpugpuValues[1][1][0]),
+                 sum(cpugpuValues[1][1][2])/len(cpugpuValues[1][1][2]),
+                 sum(cpugpuValues[1][1][1])/len(cpugpuValues[1][1][1])]
+
+    plt.bar(['15W-yolov8m', '15W-yolov8s', '15W-yolov8n', '7W-yolov8m', '7W-yolov8s', '7W-yolov8n'], 
+            mean_list,    
+            color=['red', 'blue', 'green', 'purple','yellow', 'orange'], width=0.5)
+    
+    plt.title('Consumo Medio en C++ (cpugpu)' )
+    plt.xlabel('Red y Modo Usados')
+    plt.ylabel('Consumo (mW)')
+    
+    #plt.legend()
+    plt.savefig('cppCpuGpuBarras.png')
     #plt.show()
     plt.clf()
 
@@ -228,15 +261,20 @@ def leer_datos(ruta):
     text = f.read()
 
     powerList = []
+    cpugpuList = []
 
     lines = text.split('\n')
 
     for line in lines[:-1]:
         line = line.split(' ')
 
-        powerValue = line[-1].split('/')[0][:-2]
+        powerValue = line[-5].split('/')[0][:-2]
+        cpugpuValue = line[-3].split('/')[0][:-2]
+
+        #print(cpugpuValue)
 
         powerList.append(float(powerValue))
+        cpugpuList.append(float(cpugpuValue))
 
     f.close()
 
@@ -244,41 +282,63 @@ def leer_datos(ruta):
     minV = min(powerList)
     meanV = sum(powerList) / len(powerList) 
 
-    print(maxV)
-    print(minV)
-    print(meanV)
+    print("VinMax: ", maxV)
+    print("VinMin: ", minV)
+    print("VinMean: ",meanV)
+    print("-------------------------")
+
+    maxVcg = max(cpugpuList)
+    minVcg = min(cpugpuList)
+    meanVcg = sum(cpugpuList) / len(cpugpuList)
+
+    print("VcpugpuMax: ", maxVcg)
+    print("VcpugpuMin: ", minVcg)
+    print("VcpugpuMean: ",meanVcg) 
+    print("-------------------------")
 
     #graficar_lista(powerList)
     if 'Python' in ruta:
         if '15' in ruta:
             if('yolov8m' in ruta):
                 allValues[0][0][0] = powerList
+                cpugpuValues[0][0][0] = cpugpuList
             elif('yolov8n' in ruta):
                 allValues[0][0][1] = powerList
+                cpugpuValues[0][0][1] = cpugpuList
             elif('yolov8s' in ruta):
                 allValues[0][0][2] = powerList
+                cpugpuValues[0][0][2] = cpugpuList
         elif '7' in ruta:
             if('yolov8m' in ruta):
                 allValues[0][1][0] = powerList
+                cpugpuValues[0][1][0] = cpugpuList
             elif('yolov8n' in ruta):
                 allValues[0][1][1] = powerList
+                cpugpuValues[0][1][1] = cpugpuList
             elif('yolov8s' in ruta):
                 allValues[0][1][2] = powerList
+                cpugpuValues[0][1][2] = cpugpuList
     elif 'c++' in ruta:
         if '15' in ruta:
             if('yolov8m' in ruta):
                 allValues[1][0][0] = powerList
+                cpugpuValues[1][0][0] = cpugpuList
             elif('yolov8n' in ruta):
                 allValues[1][0][1] = powerList
+                cpugpuValues[1][0][1] = cpugpuList
             elif('yolov8s' in ruta):
                 allValues[1][0][2] = powerList
+                cpugpuValues[1][0][2] = cpugpuList
         elif '7' in ruta:
             if('yolov8m' in ruta):
                 allValues[1][1][0] = powerList
+                cpugpuValues[1][1][0] = cpugpuList
             elif('yolov8n' in ruta):
                 allValues[1][1][1] = powerList
+                cpugpuValues[1][1][1] = cpugpuList
             elif('yolov8s' in ruta):
                 allValues[1][1][2] = powerList
+                cpugpuValues[1][1][2] = cpugpuList
 
 def listar_archivos_y_subcarpetas(ruta_carpeta):
     # Crear un objeto Path para la ruta dada
@@ -297,11 +357,11 @@ def listar_archivos_y_subcarpetas(ruta_carpeta):
             if(not 'time' in elemento.name ):
                 ruta = str(ruta_carpeta) + '/' + elemento.name
                 print("Archivo: " + ruta)
-                ruta_list = ruta.split('/')
+                #ruta_list = ruta.split('/')
 
-                model = ruta_list[2].split('.')[0]
-                watios = ruta_list[1]
-                if 'noidle' in ruta :
+                #model = ruta_list[2].split('.')[0]
+                #watios = ruta_list[1]
+                if 'best' in ruta :
                     leer_datos(ruta)
 
 # Ejemplo de uso
